0a41a62b16b42464e1fd529afeadc44f2fcf8e80
ebenaidoo40 <ebenaidoo40@gmail.com>
2023-08-25 09:14:40 +0000

    Title set to Codea-13

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/universal.css">
-    <title>Trip home</title>
+    <title>Codea-13</title>
 </head>
 
 
@@ -79,16 +78,6 @@
        ]);
 
        block_image_block([]);
-
-
-
-
-        
-
-
-        
-
-        
 
    ?> 
 
