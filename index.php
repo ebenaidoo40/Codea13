d8e17b3263b97d8b7e00475034d9ea9a289f568b
ebenaidoo40 <ebenaidoo40@gmail.com>
2023-10-05 22:30:35 +0000

    login function updated

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     
    <?php 
         include "reze_frame_w/a_e_xiii_library_1.0.1.php";
-
-       navbar([]);
+        navbar2([]);
+      /* 
 
        home([
         "buttonSwitch"=>"on",
@@ -46,7 +46,11 @@
 
        block_image_block([]);
 
-       slant_home([]);
+       slant_home([]);*/
+
+       login([
+        "buttonBackgroundColor"=>"slateblue",
+       ]);
 
    ?> 
 
